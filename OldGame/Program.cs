d61eda643d104be9411a749ab81d331317c166ea
Justin Williams <Justintw51@gmail.com>
2025-02-23 14:14:22 -0500

    Setting up attack and damage

--- OldGame/Program.cs
+++ OldGame/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Just typing something ");
+﻿using System;
+
+namespace OldGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerHp = 200;
+            int enemyHp = 100;
+
+            int playerAttack = 10;
+            int enemyAttack = 9;
+
+            string choice = Console.ReadLine();
+
+            if(choice == "a")
+            {
+                enemyHp -= playerAttack;
+                Console.WriteLine("You attacked the enemy! and dealt " + playerAttack + " damage ");
+            }
+        }
+    }
+}
